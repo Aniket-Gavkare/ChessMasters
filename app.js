7febab9a14ef22afb2651eecfde28d9db38d0634
root <root@LAPTOP-BJ8FH5F3>
2024-08-30 20:25:33 +0530

    most of backend part done

--- app.js
+++ app.js
@@ -29,13 +29,65 @@ app.get('/',(req,res) => {
     res.render('index');
 });
 
+//handle player connection
 io.on("connection", function(uniquesocket) {
-    console.log("connected");
-  });
+    console.log("Connected");
+
+    if(!players.white){
+        players.white = uniquesocket.id;
+        uniquesocket.emit("playrole","b");
+    }
+    else if(!players.black)
+    {
+        players.black = uniquesocket.id;
+        uniquesocket.emit("playrole","b");
+    }
+    else{
+        uniquesocket.emit("spectator");
+    }
+
+    //handle movement of pieces
+    uniquesocket.on("move",(move)=>{
+        try{
+            //check if player turn is valid
+            if(uniquesocket.id === players.white && chess.turn()!=="w") return;
+            if(uniquesocket.id === players.black && chess.turn()!=="b") return;
+
+            //try catch is for this
+            const result = chess.move(move);
+
+            if(result){
+                currentPlayer = chess.turn();
+                io.emit("move",move);
+                io.emit("boardState",chess.fen());
+            }
+            else{
+                console.log("invalid move" + move);
+                uniquesocket.emit("invalidMove",move);      
+            }
+        }
+        catch(err){
+            console.log(err);
+            uniquesocket.emit("invalidMove",move);
+        }
+    })
+
+    uniquesocket.on("disconnect",()=>{
+        if(uniquesocket.id === players.white){
+            delete players.white;
+        }
+        if(uniquesocket.id === players.black){
+            delete players.black;
+        }
+    })
+});
+
 
 server.listen(3000,function(){
     console.log("page served");
 });
+
+
 
 
 
