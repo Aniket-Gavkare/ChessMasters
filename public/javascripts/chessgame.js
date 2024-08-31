c03b39297a1f6e031656e5d594066b93f8e4d2ba
root <root@LAPTOP-BJ8FH5F3>
2024-09-01 02:19:34 +0530

    logic for renderBoard complete

--- public/javascripts/chessgame.js
+++ public/javascripts/chessgame.js
@@ -1,7 +1,7 @@
 const socket = io();
 const chess = new Chess();
 
-const chessElement = document.querySelector('.chessboard');
+const boardElement = document.querySelector('.chessboard');
 
 const draggedPrice = null;
 const sourceSquare = null;
@@ -9,7 +9,16 @@ const playerRole = null;
 
 const renderBoard = ()=>{
     const board = chess.board();
-    console.log(board);
+    boardElement.innerHTML = "";
+    board.forEach((row,rowIndex)=>{
+        row.forEach((square,squareIndex)=>{
+            const squareElement = document.createElement("div");
+            squareElement.classList.add("square",
+            (rowIndex+squareIndex) % 2 === 0 ? "light" : "dark"
+            );
+        })
+    })
+    // console.log(board);
 }
 
 const handleMove = () => {}
